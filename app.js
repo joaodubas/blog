d45b99477599ac4855e50526d23466bcaffbb826
Joao P Dubas <joao.dubas@gmail.com>
2013-07-07 22:22:46 -0300

    Configure express server and create utils to handle messaging and navigation.

---
+++ app.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+var path = require('path');
+var express = require('express');
+
+var app = module.exports = express();
+
+// serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// session support
+app.use(express.cookieParser('i4mf4tty4ndh4ppy'));
+app.use(express.session());
+
+var boot = require('boot');
+app.use(boot);
+
+app.listen(8080);
