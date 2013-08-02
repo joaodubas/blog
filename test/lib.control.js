f253bd8a03c5d43b1813994a6162cd15480d6db3
Joao P Dubas <joao.dubas@gmail.com>
2013-08-02 06:16:14 -0300

    Add tests for posts url.

--- test/lib.control.js
+++ test/lib.control.js
@@ -3,7 +3,7 @@ var request = require('supertest');
 var data = require('../lib/data.js');
 var util = require('./util');
 
-describe('home', function () {
+describe('urls', function () {
   var db = data.getDb(util.dbName);
   var app = require('../app.js');
 
@@ -37,13 +37,37 @@ describe('home', function () {
     util.clearCollection('posts', done);
   });
 
-  it('get page', function (done) {
-    request(app)
-      .get('/')
-      .expect(200)
-      .end(function (err, res) {
-        expect(res.text.match(/class="app\-article"/gi).length).to.be.equal(2);
-        done();
-      });
+  describe('home', function () {
+    it('get page', function (done) {
+      request(app)
+        .get('/')
+        .expect(200)
+        .end(function (err, res) {
+          expect(
+            res.text.match(/class="app\-article"/gi).length
+          ).to.be.equal(2);
+          done();
+        });
+    });
+  });
+
+  describe('post', function () {
+    it('get page', function (done) {
+      request(app)
+        .get('/post/post-one')
+        .expect(200)
+        .end(function (err, res) {
+          expect(res.text.match(/<article>/gi).length).to.be.equal(1);
+          done();
+        });
+    });
+    it('404 for inexistent post', function (done) {
+      request(app)
+        .get('/post/post-three')
+        .expect(404)
+        .end(function (err, res) {
+          done();
+        });
+    });
   });
 });
